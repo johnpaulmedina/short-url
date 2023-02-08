5c0abcd96dbcd55fdcdc4633dbd5e40ecd27f6fb
John Paul Medina <jp@leadtrust.io>
2023-02-07 19:50:57 -0500

    Custom console command for generating a short url

--- src/Console/Commands/BuildShortUrl.php
+++ src/Console/Commands/BuildShortUrl.php
@@ -11,7 +11,9 @@ class BuildShortUrl extends Command
      *
      * @var string
      */
-    protected $signature = 'shorturl:build';
+    protected $signature = 'shorturl:build 
+                            {url : The destination url } 
+                            {--key= : Custom key for pretty url routing }';
 
     /**
      * The console command description.
@@ -27,6 +29,20 @@ class BuildShortUrl extends Command
      */
     public function handle()
     {
-        return Command::SUCCESS;
+        $builder = new \JohnPaulMedina\ShortUrl\Classes\Builder();
+
+        $shortURLObject = $builder->destinationUrl(urldecode($this->argument('url')));
+
+        // if(request()->has('urlKey') && request()->input('urlKey')) {
+        //     $shortURLObject = $shortURLObject->urlKey(request()->input('urlKey'));
+        // }
+
+        $shortURLObject = $shortURLObject->make();
+        $shortURL = $shortURLObject->default_short_url;
+
+        $this->table(
+            ['Destination', 'Short Url'],
+            [$shortURLObject->destination, $shortURLObject->default_short_url]
+        );
     }
 }
